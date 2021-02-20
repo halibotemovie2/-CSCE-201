da152041e0e7a271e855b9d96a8dbf3f450d80ba
halibotemovie2 <77602301+halibotemovie2@users.noreply.github.com>
2021-02-20 11:43:50 +1100

    Update Driveable.java

--- Driveable.java
+++ Driveable.java
@@ -3,3 +3,40 @@ public interface Driveable {
     public void stop();
     public void turn();
 }
+
+public class Bicycle implements Driveable{
+
+    @Override
+    public void start() {
+        System.out.println("🚲自行车启动");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("🚲自行车刹车");
+    }
+
+    @Override
+    public void turn() {
+        System.out.println("🚲自行车转弯");
+    }
+}
+
+public class Car implements Driveable {
+
+    @Override
+    public void start() {
+        System.out.println("🚗汽车启动");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("🚗汽车刹车");
+    }
+
+    @Override
+    public void turn() {
+        System.out.println("🚗汽车转弯");
+    }
+}
+
