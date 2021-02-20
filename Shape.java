2f3d384d4a17da20a5572b025d5fbff5f586c5d3
halibotemovie2 <77602301+halibotemovie2@users.noreply.github.com>
2021-02-20 14:53:45 +1100

    Update Shape.java

--- Shape.java
+++ Shape.java
@@ -7,3 +7,38 @@ public class Shape {
         return 0;
     }
 }
+
+public class Rectangle extends Shape {
+    private double width, height;
+
+    public Rectangle(){
+        width=0;
+        height=0;
+    }
+    public Rectangle(double w, double h){
+        width=w;
+        height=h;
+    }
+
+    @Override
+    public double getArea(){
+        return width*height;
+    }
+
+}
+
+public class Circle extends Shape{
+    private double r;
+    public Circle(){
+        r=0;
+    }
+
+    public Circle(double r){
+        this.r=r;
+    }
+    @Override
+    public double getArea(){
+        return Math.PI*r*r;
+    }
+
+}
